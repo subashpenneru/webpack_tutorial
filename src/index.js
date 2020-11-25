e2175532ac92dc1c10a57852a6210effce688f13
Subash Penneru <subashpenneru@gmail.com>
2020-11-25 07:49:36 +0530

    plugins completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
+import Heading from './components/heading/heading';
 
 const hwBtn = new HelloWorldButton();
+const heading = new Heading();
 
+heading.render();
 hwBtn.render();
